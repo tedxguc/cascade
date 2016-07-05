7a4e24c2ae1523639c422f8cd9809464a8f996c3
Omar Kamal <omarkamal95@gmail.com>
2016-07-05 22:39:44 +0200

    removed abide

--- js/main.js
+++ js/main.js
@@ -1,39 +1,39 @@
 $(function() {
 
-    $(document).foundation();
+    // $(document).foundation();
 
-    var lastScrollTop = 0,
-        delta = 5;
+    // var lastScrollTop = 0,
+    //     delta = 5;
 
-    var $c0 = $('.c0');
-    var $c1 = $('.c1');
-    var $c2 = $('.c2');
+    // var $c0 = $('.c0');
+    // var $c1 = $('.c1');
+    // var $c2 = $('.c2');
 
-    $(window).scroll(function() {
-        var nowScrollTop = $(this).scrollTop();
-        if (Math.abs(lastScrollTop - nowScrollTop) >= delta) {
-            if (nowScrollTop > lastScrollTop) {
-                // ACTION ON
-                // SCROLLING DOWN 
-                // console.log(lastScrollTop);
-                // var prop = 'translateY(' + -lastScrollTop + ')';
-                // $c1.css({
-                //     '-webkit-transform': prop,
-                //     '-moz-transform': prop,
-                //     '-ms-transform': prop,
-                //     '-o-transform': prop,
-                //     'transform': prop
+    // $(window).scroll(function() {
+    //     var nowScrollTop = $(this).scrollTop();
+    //     if (Math.abs(lastScrollTop - nowScrollTop) >= delta) {
+    //         if (nowScrollTop > lastScrollTop) {
+    //             // ACTION ON
+    //             // SCROLLING DOWN 
+    //             // console.log(lastScrollTop);
+    //             // var prop = 'translateY(' + -lastScrollTop + ')';
+    //             // $c1.css({
+    //             //     '-webkit-transform': prop,
+    //             //     '-moz-transform': prop,
+    //             //     '-ms-transform': prop,
+    //             //     '-o-transform': prop,
+    //             //     'transform': prop
 
-                // });
+    //             // });
 
-            } else {
-                // ACTION ON
-                // SCROLLING UP 
-                // console.log('up');
-            }
-            lastScrollTop = nowScrollTop;
-        }
-    });
+    //         } else {
+    //             // ACTION ON
+    //             // SCROLLING UP 
+    //             // console.log('up');
+    //         }
+    //         lastScrollTop = nowScrollTop;
+    //     }
+    // });
 
     var textarea = document.querySelector('textarea');
     textarea.addEventListener('keydown', autosize);
